3135f64dbee9da04580c1e499990bb1f59427c29
anthony <anthony.steffen@outlook.com.br>
2024-10-19 09:11:32 -0300

    :sparkles: feat: Encrypts passwords before registering on DB

--- back-end/models/usuario.js
+++ back-end/models/usuario.js
@@ -1,22 +1,22 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => sequelize.define('Usuario', {
-  nome: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: {
-      msg: 'Este nome de usuário já está em uso.'
+const bcrypt = require('bcryptjs');
+
+module.exports = (sequelize, DataTypes) => {
+  const Usuario = sequelize.define('Usuario', {
+    nome: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: {
+        msg: 'Este nome de usuário já está em uso.'
+      }
     },
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: {
-      msg: 'Este e-mail já está cadastrado.'
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: {
+        msg: 'Este e-mail já está cadastrado.'
+      }
     },
-  },
     senha: {
       type: DataTypes.STRING,
       allowNull: false,
@@ -24,12 +24,32 @@ module.exports = (sequelize, DataTypes) => sequelize.define('Usuario', {
         is: {
           args: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/,
           msg: 'A senha precisa ter no mínimo 8 caracteres, uma letra maiúscula, uma letra minúscula e um número.'
+        }
       }
     }
-  }
   }, {
-    modelName: 'Usuario',
     tableName: 'usuarios',
     underscored: true,
     timestamps: false,
+    hooks: {
+      // Criptografa a senha antes de salvar no banco
+      beforeCreate: async (usuario) => {
+        const salt = await bcrypt.genSalt(10);
+        usuario.senha = await bcrypt.hash(usuario.senha, salt);
+      },
+      beforeUpdate: async (usuario) => {
+        if (usuario.changed('senha')) {
+          const salt = await bcrypt.genSalt(10);
+          usuario.senha = await bcrypt.hash(usuario.senha, salt);
+        }
+      }
+    }
   });
+
+  // Método para validar a senha durante o login
+  Usuario.prototype.validPassword = async function (senha) {
+    return bcrypt.compare(senha, this.senha);
+  };
+
+  return Usuario;
+};
