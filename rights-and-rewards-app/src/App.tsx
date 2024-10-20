dd1f6b410620ef7cbfa2ae1b9104545866ed339e
anthony <anthony.steffen@outlook.com.br>
2024-10-20 10:51:00 -0300

    :heavy_plus_sign:	build: Installing Chakra UI and configuring routes for Login, Register and Dashboard pages

--- rights-and-rewards-app/src/App.tsx
+++ rights-and-rewards-app/src/App.tsx
@@ -1,14 +1,25 @@
 import './App.css'
+import { Text, Box, ChakraProvider } from '@chakra-ui/react'
 
 function App() {
 
   return (
-    <>
-      App
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <ChakraProvider>
+      <Box 
+      width={["100%", "50%", "25%"]} p={[2, 4, 6]}
+      color='red.50'
+      background={['red.100', 'green.100', 'blue.500']}
+      style={{border: '1px solid black'}}
+      >
+        <Text fontSize="xl">Hello, World</Text>
+      </Box>
+    </ChakraProvider>
+    // <div style={{backgroundColor: 'lightblue', padding: '20px'}}>
+    //   App
+    //   <p className="read-the-docs">
+    //     Click on the Vite and React logos to learn more
+    //   </p>
+    // </div>
   )
 }
 
--- rights-and-rewards-app/src/main.tsx
+++ rights-and-rewards-app/src/main.tsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App.tsx'
+import Login from './pages/login/Login.tsx'
+import Register from './pages/register/Register.tsx'
 import './index.css'
+import Dashboard from './pages/dashboard/Dashboard.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <Router basename="/rights-and-rewards-app">
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<Dashboard/>} />
+      </Routes>
+    </Router>
+  </StrictMode>
 )
